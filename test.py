a72d01a8538b0042d078f81f9767aaf109e8d84d
a1935af <49624948+a1935af@users.noreply.github.com>
2020-04-27 19:47:20 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import pytest                                                                                                                                
 
 def test_add_01():
-    assert "a" == "b"
+    assert "a" == "a"
 
 
